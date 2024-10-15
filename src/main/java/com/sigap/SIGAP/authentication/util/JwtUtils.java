80b5eb8443e006e4d483fae736690eb898e9a672
JOAN <joanandres10@gmail.com>
2024-10-15 14:18:50 -0500

    se crea carpeta jwtUtils

---
+++ src/main/java/com/sigap/SIGAP/authentication/util/JwtUtils.java
@@ -0,0 +1,74 @@
+package com.sigap.SIGAP.authentication.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+
+    //autenticidad de los token
+    @Value("${security.jwt.key.private}")
+    private String privatekey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+//metodo de utilerias
+
+    public String createToken(Authentication authentication) {
+        //algoritmo de incrptacion Hmac es el mas comun
+        Algorithm algorithm = Algorithm.HMAC256(this.privatekey);
+
+        String username = authentication.getPrincipal().toString();
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));//READ,WRITE.UPDATE
+
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+        return jwtToken;
+    }
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privatekey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT){
+        return decodedJWT.getSubject().toString();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+}
